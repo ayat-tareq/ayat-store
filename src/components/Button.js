0d351c08f3109ef073c6c77e1cd045b4d54685c4
ayat tareq <48443344+ayat-tareq@users.noreply.github.com>
2020-10-19 21:36:26 +0300

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 export const ButtonContainer=styled.button`
 text-transform:capitalize;
-font-size:1.4rem;
+font-size:0.9rem;
 background:transparent;
 border:0.05rem solid var(--lightBlue) ;
 border-color:${props=>props.cart ? "var(--mainYellow)":"var(--lightBlue)"};
